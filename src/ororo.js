4d0c213cb7b36b3e30c1b35db252b3a46ca29e86
egorgrushin <egorgrushin@gmail.com>
2024-06-02 21:17:04 +0300

    fixes

--- src/ororo.js
+++ src/ororo.js
@@ -1,9 +1,16 @@
 export const createOroroApi = () => {
     const baseUrl = 'https://front.ororo-mirror.tv/api/v2';
+    const token = 'ZWdvcmdydXNoaW5AZ21haWwuY29tOkxhbGFlcHQy';
+    const authorization = `Basic ${token}`;
 
     const request = (url) => {
         const fullUrl = `${baseUrl}/${url}`;
-        return fetch(fullUrl).then((response) => response.json());
+
+        return fetch(fullUrl, {
+            headers: {
+                Authorization: authorization,
+            },
+        }).then((response) => response.json());
     };
 
     const getShows = () => request('shows');
