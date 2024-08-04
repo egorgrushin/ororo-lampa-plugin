175445951076f2f45fbac6cc1ba084df4a447142
egorgrushin <egorgrushin@gmail.com>
2024-08-05 01:12:18 +0300

    fixes

--- src/ororo.js
+++ src/ororo.js
@@ -1,14 +1,14 @@
 export const createOroroApi = () => {
     const baseUrl = 'https://front.ororo-mirror.tv/api/v2';
-    const token = 'dadadasdas';
-    const authorization = `Basic ${token}`;
+    const token =
+        'eyJfcmFpbHMiOnsibWVzc2FnZSI6Ilcxc3lNVGcyTnpZMFhTd2ljRmd0VFhKcVFVVkJUREoxUVZkblFUYzVXRUVpTENJeE56SXlPRGd3TXpjM0xqQXpNVGt4TXlKZCIsImV4cCI6IjIwMjUtMDgtMDVUMTc6NTI6NTcuMDMxWiIsInB1ciI6ImNvb2tpZS5yZW1lbWJlcl91c2VyX3Rva2VuIn19--bd67f7517e43f19b2747c7b6d33c525a50ca8132';
 
     const request = (url) => {
         const fullUrl = `${baseUrl}/${url}`;
 
         return fetch(fullUrl, {
             headers: {
-                Authorization: authorization,
+                'Set-cookie': `remember_user_token=${token}`,
             },
         }).then((response) => response.json());
     };
