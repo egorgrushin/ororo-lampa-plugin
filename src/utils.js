22b0657db539cf3120fec26a0ef820657fa1aef0
egorgrushin <egorgrushin@gmail.com>
2024-08-06 00:24:14 +0300

    fixes

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
 export const getCurrentActivity = () => Lampa.Activity.active().activity;
 export const translate = (key) => Lampa.Lang.translate(key);
 
-export const getCurrentLanguage = () => Storage.get('language', 'ru');
+export const getCurrentLanguage = () => Lampa.Storage.get('language', 'ru');
