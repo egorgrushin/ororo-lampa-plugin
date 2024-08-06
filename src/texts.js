d63a7c18b14f7aa9195cc30434daeb49ef843f48
egorgrushin <egorgrushin@gmail.com>
2024-08-07 01:38:07 +0300

    fixes

--- src/texts.js
+++ src/texts.js
@@ -5,6 +5,7 @@ export const TEXTS = {
     EmptyFilter: `${PLUGIN_NAME}-empty-filter`,
     SeasonTitle: `${PLUGIN_NAME}-season-title`,
     NoOroroShow: `${PLUGIN_NAME}-no-ororo-show`,
+    NoOroroAccess: `${PLUGIN_NAME}-no-ororo-access`,
     Oops: `${PLUGIN_NAME}-oops`,
 };
 
@@ -14,6 +15,10 @@ export const registerTexts = () => {
         [TEXTS.EmptyFilter]: { ru: 'Empty', en: 'Empty' },
         [TEXTS.SeasonTitle]: { ru: 'Сезон', en: 'Season' },
         [TEXTS.NoOroroShow]: { ru: 'Этого сериала нет на ororo.tv', en: 'This show is not listed in ororo.tv' },
+        [TEXTS.NoOroroAccess]: {
+            ru: 'Нет доступа на ororo.tv. Пожалуйста, проверьте настройки ororo-плагина и попробуйте снова',
+            en: 'Access denied to ororo.tv. Please, check ororo-plugin settings and try again',
+        },
         [TEXTS.Oops]: { ru: 'Упс', en: 'Oops' },
     });
 };
