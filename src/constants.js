6e6b02f5784f54004d0cc802683f0cef6b0fedeb
egorgrushin <egorgrushin@gmail.com>
2024-08-07 19:18:57 +0300

    fixes

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,8 @@
 export const PLUGIN_NAME = 'ororo';
 export const COMPONENT_NAME = 'ororo';
+export const SETTINGS_COMPONENT_NAME = `${PLUGIN_NAME}-settings`;
 export const OPEN_BUTTON_ID = `${PLUGIN_NAME}-open-button`;
+export const IS_PLUGIN_READY = `${PLUGIN_NAME}PluginIsReady`;
+export const IS_PLUGIN_SETTINGS_READY = `${PLUGIN_NAME}PluginSettingsIsReady`;
 export const FILTER_KEY = `filter`;
 export const CONTENT_CONTROLLER_NAME = `content`;
-
