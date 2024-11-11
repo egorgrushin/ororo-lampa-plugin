dc2f2e7c15a2b26833bc49a4b5e18faeb86f1300
egorgrushin <egorgrushin@gmail.com>
2024-11-11 20:19:42 +0300

    fixes

--- src/index.js
+++ src/index.js
@@ -77,6 +77,7 @@ const addSettings = () => {
 const initOroroApi = () => {
     const login = Lampa.Storage.field(LOGIN_SETTING_PARAM);
     const password = Lampa.Storage.field(PASSWORD_SETTING_PARAM);
+    debugger;
     window[ORORO_API_KEY] = createOroroApi(login, password);
 };
 
